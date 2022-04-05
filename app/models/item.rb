77c39f89e93ff11405d5896d84949034adaa4c81
hm19890304 <hm19890304@icloud.com>
2022-04-05 17:42:21 +0900

    categoryモデルの作成およびアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
 end

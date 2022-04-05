776cec0e97457bf1574a3f83b3b9b295c6adfba2
hm19890304 <hm19890304@icloud.com>
2022-04-05 17:52:35 +0900

    delivery_chargeモデルの作成およびアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,11 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
+  belongs_to :delivery_charge
 
   with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :status_id
+    validates :delivery_charge_id
   end
 end

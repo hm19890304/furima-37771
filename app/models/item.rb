2fa43e48b43fbda5595156b3e4d58313a2687611
hm19890304 <hm19890304@icloud.com>
2022-04-05 17:56:22 +0900

    prefectureモデルの作成およびアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,12 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :status
   belongs_to :delivery_charge
+  belongs_to :prefecture
 
   with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :status_id
     validates :delivery_charge_id
+    validates :prefecture_id
   end
 end

20ab1b8959e168d538b516e570a12594dce0c04e
hm19890304 <hm19890304@icloud.com>
2022-04-05 18:03:29 +0900

    shipping_dateモデルの作成およびアソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,13 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :delivery_charge
   belongs_to :prefecture
+  belongs_to :shipping_date
 
   with_options numericality: { other_than: 1 , message: "can't be blank"} do
     validates :category_id
     validates :status_id
     validates :delivery_charge_id
     validates :prefecture_id
+    validates :shipping_date_id
   end
 end

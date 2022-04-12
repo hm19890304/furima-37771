2ea675b25048122c3142d1e6f466d8cef12f466b
hm19890304 <hm19890304@icloud.com>
2022-04-12 18:29:38 +0900

    アソシエーションの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, :price, :image)
+    params.require(:item).permit(:name, :explanation, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end

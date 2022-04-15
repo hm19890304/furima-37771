90cfc1dab67618820846299ab530578c2145fe6c
hm19890304 <hm19890304@icloud.com>
2022-04-15 18:58:59 +0900

    buyerモデルの作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  has_one :buyer
+
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end

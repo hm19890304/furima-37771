178cf473f784b69f7a68548759c7c646bc6b2c52
hm19890304 <hm19890304@icloud.com>
2022-04-15 18:25:54 +0900

    orderモデルの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,10 @@
 class OrdersController < ApplicationController
   def index
-    
+    @order = Order.new(order_params)
+  end
+
+  private
+  def order_params
+    params.permit(:user_id, :item_id)
   end
 end

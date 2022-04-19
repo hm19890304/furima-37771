8eaceda97b391e1894b3280ba28be0fc69b83913
hm19890304 <hm19890304@icloud.com>
2022-04-19 18:37:12 +0900

    テストコードの追記

--- spec/factories/order_buyers.rb
+++ spec/factories/order_buyers.rb
@@ -7,5 +7,6 @@ FactoryBot.define do
     address { Faker::Address.street_name }
     building_name { Faker::Address.building_number }
     phone_number { Faker::PhoneNumber.subscriber_number(length: 11) }
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
--- spec/models/order_buyer_spec.rb
+++ spec/models/order_buyer_spec.rb
@@ -80,6 +80,11 @@ RSpec.describe OrderBuyer, type: :model do
         @order_buyer.valid?
         expect(@order_buyer.errors.full_messages).to include("Item can't be blank")
       end
+      it "tokenが空では登録できないこと" do
+        @order_buyer.token = nil
+        @order_buyer.valid?
+        expect(@order_buyer.errors.full_messages).to include("Token can't be blank")
+      end
     end
   end
 end
